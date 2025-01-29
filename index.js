946b5554103688c3222a05a5175fa9d3ec663d93
elale1984 <edward.gratefuldead1@gmail.com>
2025-01-29 14:34:05 -0600

    finished

--- index.js
+++ index.js
@@ -12,6 +12,27 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 })
 
+app.get("/api/:date?", (req, res) => {
+  let dateParam;
+
+  if (!req.params.date) {
+    dateParam = new Date();
+  } else {
+    const timestamp = Number(req.params.date);
+    dateParam = isNaN(timestamp) ? new Date(req.params.date) : new Date(timestamp);
+  }
+
+  if (isNaN(dateParam.getTime())) {
+    return res.json({ error: "Invalid Date" });
+  }
+
+  res.json({
+    unix: dateParam.getTime(),
+    utc: dateParam.toUTCString(),
+  });
+});
+
+
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
